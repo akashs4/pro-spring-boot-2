cef27d5797804373f544f2316e2a8cc5b912150b
Singh-Akash <singh.akash@247.ai>
2022-04-28 09:46:52 +0530

    excluding class from auto configuration

--- ch02/src/main/java/com/example/demo/DemoApplication.java
+++ ch02/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ActiveMQAutoConfiguration.class, DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
